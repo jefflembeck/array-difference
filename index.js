ae849996f24e205908a1b2c2a80f984569c70227
Jeffrey Lembeck <jlembeck@gmail.com>
2016-02-08 10:46:42 -0800

    Assert considered unsafe
    And let's be honest, totally unnecessary here.

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-var assert = require('assert');
-
 module.exports = function(a, b){
-  assert(Array.isArray(a) && Array.isArray(b), "Both items passed must be of type Array");
-
+  if(!(Array.isArray(a) && Array.isArray(b))){
+    throw new Error("Both items passed must be of type Array");
+  }
   const bSet = new Set(b);
   return a.filter((x) => !bSet.has(x));
 };
