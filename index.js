43cd3df881e7791ab52d1b48186d68a844e4816e
Jeffrey Lembeck <jlembeck@gmail.com>
2016-02-08 09:57:16 -0800

    ES6 is fun
    Use const and arrow functions

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ var assert = require('assert');
 module.exports = function(a, b){
   assert(Array.isArray(a) && Array.isArray(b), "Both items passed must be of type Array");
 
-  var universe = new Set(a.concat(b));
-  var bSet = new Set(b);
-  return Array.from(universe).filter(function(x){
-    return !bSet.has(x);
-  });
+  const universe = new Set(a.concat(b));
+  const bSet = new Set(b);
+  return Array.from(universe).filter((x) => !bSet.has(x));
 };
